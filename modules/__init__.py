e95227d7a1e6cf9d1527ada69c5352394859dcde
SergioAndresG <140452759+SergioAndresG@users.noreply.github.com>
2025-11-24 16:45:05 -0500

    fat(__init__.py): Paquete de módulos funcionales

---
+++ modules/__init__.py
@@ -0,0 +1,4 @@
+from .authentication import AuthenticationModule
+from .navigation import NavigationModule
+from .data_extraction import DataExtractionModule
+from .export_handler import ExportHandler
