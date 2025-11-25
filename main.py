4d58382d735d0fe4dabc892158a3b527ef3d1652
SergioAndresG <140452759+SergioAndresG@users.noreply.github.com>
2025-11-25 15:17:34 -0500

    feat(): Actualiza solo para lanzar la aplicación, mientras la vista llama la logica de negacio

--- main.py
+++ main.py
@@ -1,55 +1,8 @@
-"""
-Script principal para ejecutar la extracción de datos
-"""
-import traceback
+from gui.main_window import App
 
-from config import configurar_logging
-from core import ExtractorDatosEmpresa
-from urls.urls import URL_LOGIN
-
-
-def main():
-    """Función principal"""
-    try:
-        # Configurar logging
-        configurar_logging()
-        
-        # Lista de municipios a procesar
-        municipios = [
-            "Mosquera",
-            "Madrid",
-            "Bojacá",
-            "Zipacón",
-            "Facatativá",
-            "El Rosal",
-            "Subachoque",
-            "Tabio",
-            "Cota",
-            "Funza",
-            "Tenjo",
-            "Guasca",
-            "Gachetá",
-            "Gama",
-            "Ubalá",
-            "Gachalá",
-            "Junín",
-        ]
-        
-        print("Iniciando el proceso de extracción de datos de empresas...")
-        
-        # Crear instancia del extractor
-        extractor = ExtractorDatosEmpresa()
-        
-        # Ejecutar proceso completo
-        extractor.ejecutar_proceso_completo(municipios, URL_LOGIN)
-        
-    except KeyboardInterrupt:
-        print("\n Proceso interrumpido por el usuario")
-        
-    except Exception as e:
-        print(f" Error inesperado: {str(e)}")
-        traceback.print_exc()
-
+def run_app():
+    app = App()
+    app.mainloop()
 
 if __name__ == "__main__":
-    main()
+    run_app()
