6bf5cfa3949a6b55eae6d9ddfd418445ead988a4
SergioAndresG <140452759+SergioAndresG@users.noreply.github.com>
2025-11-25 14:20:20 -0500

    fix(): Configuración completa de los municipios de cobertura

---
+++ config/municipalities.py
@@ -0,0 +1,9 @@
+# Lista de municipios a procesar
+municipios_cobertura = [
+    "Mosquera","Madrid","Bojacá",
+    "Zipacón","Facatativá","El Rosal",
+    "Subachoque","Tabio","Cota",
+    "Funza","Tenjo","Guasca",
+    "Gachetá","Gama","Ubalá",
+    "Gachalá","Junín",
+]
