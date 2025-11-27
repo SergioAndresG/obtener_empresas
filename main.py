dc3d22ccc943a40b587a655a54ef6fc5bc8b1267
SergioAndresG <140452759+SergioAndresG@users.noreply.github.com>
2025-11-26 20:59:37 -0500

    feat(): Se asegura de que antes de crear la app, se configuren las credenciales

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from gui.main_window import App
+from config import settings
+from utils.credentials import CredentialsManager
 
 def run_app():
     app = App()
     app.mainloop()
+    # Configurar credentials manager ANTES de crear la app
+    credentials_manager = CredentialsManager()
+    settings.set_credentials_manager(credentials_manager)
 
 if __name__ == "__main__":
     run_app()
